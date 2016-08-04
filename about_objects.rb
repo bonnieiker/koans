347c67246e07d78a53e20f375bab9e3c52d7ee5a
Bonnie Iker <bonniejiker@gmamil.com>
2016-08-03 22:58:59 -0400

    completed these koans

--- about_objects.rb
+++ about_objects.rb
@@ -2,21 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutObjects < Neo::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
-    assert_equal __, 1.5.is_a?(Object)
-    assert_equal __, "string".is_a?(Object)
-    assert_equal __, nil.is_a?(Object)
-    assert_equal __, Object.is_a?(Object)
+    assert_equal _true_, 1.is_a?(Object)
+    assert_equal _true_, 1.5.is_a?(Object)
+    assert_equal _true_, "string".is_a?(Object)
+    assert_equal _true_, nil.is_a?(Object)
+    assert_equal _true_, Object.is_a?(Object)
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal __, 123.to_s
-    assert_equal __, nil.to_s
+    assert_equal "123"__, 123.to_s
+    assert_equal _""_, nil.to_s
   end
 
   def test_objects_can_be_inspected
-    assert_equal __, 123.inspect
-    assert_equal __, nil.inspect
+    assert_equal _"123"_, 123.inspect
+    assert_equal _"nil"_, nil.inspect
   end
 
   def test_every_object_has_an_id
@@ -27,14 +27,14 @@ class AboutObjects < Neo::Koan
   def test_every_object_has_different_id
     obj = Object.new
     another_obj = Object.new
-    assert_equal __, obj.object_id != another_obj.object_id
+    assert_equal _true_, obj.object_id != another_obj.object_id
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal _1_, 0.object_id
+    assert_equal _3_, 1.object_id
+    assert_equal _5_, 2.object_id
+    assert_equal _201_, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
@@ -44,7 +44,7 @@ class AboutObjects < Neo::Koan
     obj = Object.new
     copy = obj.clone
 
-    assert_equal __, obj           != copy
-    assert_equal __, obj.object_id != copy.object_id
+    assert_equal true__, obj           != copy
+    assert_equal _true_, obj.object_id != copy.object_id
   end
 end
